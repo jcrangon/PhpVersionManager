6e5471974e59e8b69e78578567a28b5512868822
jc rangon <jc.rangon@gmail.com>
2023-10-10 20:35:08 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace PhpVersionManager
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (!Convert.ToBoolean(args.Length))
+            {
+                Console.ReadKey(true);
+
+            } else
+            {
+                switch (args[0])
+                {
+                    case "list":
+                        if (args[1].Length != 0 && args[1] == "available")
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Listing all available versions");
+                            Web.Harvester();
+
+                        }
+                        break;
+                    case "ls":
+                        Console.WriteLine("");
+                        Console.WriteLine("Listing all installed versions...");
+                        await Utils.ListVersions();   
+                        break;
+                    case "install":
+                        if (args[1].Length != 0)
+                        {
+                            string version = args[1];
+                            Console.WriteLine("");
+                            Console.WriteLine($"Installing version {version}");
+                             await Downloader.PhpDownload(version);
+                        }
+                        break;
+                    case "use":
+                        if (args[1].Length != 0)
+                        {
+                            Console.WriteLine("");
+                            string version = args[1];
+                            Console.WriteLine($"Activating version {version}...");
+                            await Utils.setActiveVersion(version);
+                        }
+                        break;
+                    case "remove":
+                        if (args[1].Length != 0)
+                        {
+                            Console.WriteLine("");
+                            string version = args[1];
+                            Console.WriteLine($"Removing version {version}...");
+                            await Utils.RemoveVersion(version);
+                        }
+                        break;
+
+                    case "-help":
+                        Console.WriteLine("");
+                        Console.WriteLine("Printing PVM help:");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
